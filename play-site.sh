6860389876c81f023939eb12e24804ce82d7ef96
Xiaodong Xu <xxdlhy@gmail.com>
2017-03-06 13:26:39 +0800

    Update play-site.sh

--- play-site.sh
+++ play-site.sh
@@ -2,10 +2,11 @@
 
 set -e
 
-echo
-echo -e "\033[38;5;255m\033[48;5;234m\033[1m  S E L F H O S T E D - S E R V E R  \033[0m"
-echo
-
+echo -e "
+┏━┓┏━╸╻  ┏━╸╻ ╻┏━┓┏━┓╺┳╸┏━╸╺┳┓   ┏━┓┏━╸┏━┓╻ ╻┏━╸┏━┓
+┗━┓┣╸ ┃  ┣╸ ┣━┫┃ ┃┗━┓ ┃ ┣╸  ┃┃   ┗━┓┣╸ ┣┳┛┃┏┛┣╸ ┣┳┛
+┗━┛┗━╸┗━╸╹  ╹ ╹┗━┛┗━┛ ╹ ┗━╸╺┻┛   ┗━┛┗━╸╹┗╸┗┛ ┗━╸╹┗╸
+"
 echo -n "Which provider are you using?
   1. DigitalOcean
   2. Linode
@@ -18,5 +19,5 @@ case "$reply" in
   1) ansible-playbook site-do.yml;;
   2) ansible-playbook site-linode.yml;;
   3) ansible-playbook site-vultr.yml;;
-  *) echo "Invalid provider selected."; exit 1;;
+  *) echo "Invalid provider selected"; exit 1;;
 esac
