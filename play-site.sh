16b7c52a03d62feaac6b348d3a1a11604a35735b
Xiaodong Xu <xxdlhy@gmail.com>
2017-03-11 13:47:32 +0800

    gce: fix variable problem

--- play-site.sh
+++ play-site.sh
@@ -11,6 +11,7 @@ echo -n "Which provider are you using?
   1. DigitalOcean
   2. Linode
   3. Vultr
+  4. Google Computer Engine
 Your choose (number): "
 
 read reply
@@ -19,5 +20,6 @@ case "$reply" in
   1) ansible-playbook site-do.yml;;
   2) ansible-playbook site-linode.yml;;
   3) ansible-playbook site-vultr.yml;;
+  4) ansible-playbook site-gce.yml;;
   *) echo "Invalid provider selected"; exit 1;;
 esac
